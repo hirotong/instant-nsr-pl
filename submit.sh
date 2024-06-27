c9b6aa9a7091b64f5897f4ec9558d515dce67e82
hiro <jinguang.tong@anu.edu.cn>
2024-06-27 17:00:06 +1000

    update

--- submit.sh
+++ submit.sh
@@ -1,6 +1,6 @@
 for case in $(ls ./load/nerf_synthetic); do
-    sbatch batch_train.sh configs/neus-rotblender.yaml $case womask_larger_sample_region
-    sbatch batch_train.sh configs/neus-rotblender-wmask.yaml $case wmask_larger_sample_region
-    sbatch batch_train.sh configs/rotneus-rotblender-wmask.yaml $case wmask_larger_sample_region
-    sbatch batch_train.sh configs/rotneus-rotblender.yaml $case womask_larger_sample_region
+    # sbatch batch_train.sh configs/neus-rotblender.yaml $case womask_larger_sample_region
+    # sbatch batch_train.sh configs/neus-rotblender-wmask.yaml $case wmask_larger_sample_region
+    sbatch batch_train.sh configs/rotneus-rotblender-wmask.yaml $case wmask_larger_region_more_sample
+    sbatch batch_train.sh configs/rotneus-rotblender.yaml $case womask_larger_region_more_sample
 done
